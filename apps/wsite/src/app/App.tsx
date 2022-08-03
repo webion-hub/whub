aab7459f6f48a52e85e0f600d15d3a74e7032039
Matteo Budriesi <68251639+matteo2437@users.noreply.github.com>
2022-08-03 17:08:38 +0200

    Locales (#30)
    * fix footer
    
    * add facebook pixel and locales
    
    * fix translation
    
    * remove comment
    
    * fix language bug
    
    Co-authored-by: AlessandroDodi <96247758+AlessandroDodi@users.noreply.github.com>

---
+++ apps/wsite/src/app/App.tsx
@@ -0,0 +1,66 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import en from "../assets/locales/en-EN.json";
+import it from "../assets/locales/it-IT.json";
+
+import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from './components/layout/Layout';
+
+import Homepage from './pages/home-page/Homepage';
+import theme from './theme/theme'
+import { GlobalStyles, CssBaseline, CircularProgress, Grid} from '@mui/material';
+import globalStyle from './theme/globalStyle';
+import PoliciesAndLicensesPage from './pages/policies-licenses-page/PoliciesAndLicensesPage';
+import { useState } from "react";
+import { MaybeShow } from "@whub/wui";
+
+export function App() {
+  const [loading, setLoading] = useState(true)
+
+  i18n
+    .use(initReactI18next)
+    .init({
+      resources: {
+        en: { translation: en },
+        it: { translation: it }
+      },
+
+      interpolation: {
+        escapeValue: false
+      }
+    })
+    .finally(() => setLoading(false))
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <GlobalStyles styles={globalStyle}></GlobalStyles>
+      <MaybeShow
+        show={!loading}
+        alternativeChildren={
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{height: '100vh'}}
+          >
+            <CircularProgress/>
+          </Grid>
+        }
+      >
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route key="home" path="/"  element={<Homepage/>}/>
+              <Route key="policies" path="/policies-licenses" element={<PoliciesAndLicensesPage/>}/>
+              <Route key="all" path="/*" element={<Homepage/>}/>
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </MaybeShow>
+    </ThemeProvider>
+  )
+}
--- apps/wsite/src/main.tsx
+++ apps/wsite/src/main.tsx
@@ -1,21 +1,13 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@mui/material/styles';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from './app/components/layout/Layout';
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
 
-import en from "./assets/locales/en-EN.json";
-import it from "./assets/locales/it-IT.json";
+import ReactPixel from 'react-facebook-pixel';
+import { App } from './app/App';
 
 import './styles.css'
 
-import Homepage from './app/pages/home-page/Homepage';
-import theme from './app/theme/theme'
-import { GlobalStyles, CssBaseline} from '@mui/material';
-import globalStyle from './app/theme/globalStyle';
-import PoliciesAndLicensesPage from './app/pages/policies-licenses-page/PoliciesAndLicensesPage';
+
+ReactPixel.init('1771971246478638');
 
 import ReactPixel from 'react-facebook-pixel';
 
@@ -25,35 +17,9 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-i18n
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: { translation: en },
-      it: { translation: it }
-    },
-    lng: "it",
-
-    interpolation: {
-      escapeValue: false
-    }
-  });
-
 
 root.render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline/>
-      <GlobalStyles styles={globalStyle}></GlobalStyles>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route key="home" path="/"  element={<Homepage/>}/>
-            <Route key="policies" path="/policies-licenses" element={<PoliciesAndLicensesPage/>}/>
-            <Route key="all" path="/*" element={<Homepage/>}/>
-          </Routes>
-        </Layout>
-      </BrowserRouter>
-    </ThemeProvider>
+    <App/>
   </StrictMode>
 );
