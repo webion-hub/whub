80dc1d185f3f1e89764ae42622b55dd0ba26dd05
Davide Messori <davide.messori@outlook.com>
2022-08-04 14:25:54 +0200

    Extracted WuiFooter

--- apps/wsite/src/app/components/layout/WebionFooter.tsx
+++ apps/wsite/src/app/components/layout/WebionFooter.tsx
@@ -1,184 +1,86 @@
-import { Button, ButtonProps, Divider, Grid, IconButton, Link, Typography } from "@mui/material";
-
+import { Img, sectionProps, WuiFooter } from "@whub/wui";
 import { useTranslation } from "react-i18next";
-import React from "react";
+
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { Footer, FooterBottomLabel, FooterColumn, FooterContent, Img } from "@whub/wui";
 
-const FooterBtn = (props: ButtonProps) => {
-  return (
-    <Button
-      {...props}
-      sx={{
-        ...props.sx,
-        alignSelf: "auto",
-        width: "fit-content",
-        minWidth: 0,
-        marginLeft: theme => ({
-          sm: theme.spacing(-1, '!important'), 
-          xs: 0
-        })
-      }}
-    >
-      {props.children}
-    </Button>
+export default function WebionFooter() {
+  const {t} = useTranslation()
+  const sections: sectionProps[] = [
+    {
+      title: "Home",
+      buttons: 
+        [
+          {
+            text: t("navbar-button1"),
+            href: "/#home",
+          },
+          {
+            text: t("navbar-button2"),
+            href: "/#services",
+          },
+          {
+            text: t("navbar-button3"),
+            href: "/#projects",
+          },
+          {
+            text: t("navbar-button5"),
+            href: "/#contacts",
+          },
+      ]
+    },
+    {
+      title: t("contact-us-title"),
+      buttons: 
+        [
+          {
+            text: "via Panfilo Castaldi 3, Modena",
+            onClick: () => window.open("http://maps.google.com/?q=Webion SRL", '_blank')?.focus()
+          },
+          {
+            text: "+39 389 008 6632",
+            href: "tel:+39 389 008 6632",
+          },
+          {
+            text: "amministrazione@webion.it",
+            href: "mailto:amministrazione@webion.it",
+          },
+      ]
+    },
+  ]
+
+  const socials = [
+    {
+      href: "https://www.facebook.com/Webion-107914721922394",
+      icon: <FacebookIcon fontSize="inherit" />
+    },
+    {
+      href: "https://www.quora.com/profile/Webion",
+      icon: <Img width="25px" height="20px" src="/assets/images/quoraIcon.svg"/>
+    },
+    {
+      href: "https://www.instagram.com/webion.it/",
+      icon: <InstagramIcon fontSize="inherit" />
+    },
+    {
+      href: "https://www.linkedin.com/company/webion-srl/about/",
+      icon: <LinkedInIcon fontSize="inherit" />
+    },
+    {
+      href: "https://github.com/webion-hub",
+      icon: <GitHubIcon fontSize="inherit" />
+    },
+  ]
+
+  return(
+    <WuiFooter 
+      sectionsProps={sections} 
+      socialsProps={socials}
+      privacyLink={t("privacy-link")}
+      extraText={t("p-iva")}
+      mailLink="webionsrl@legalmail.it"
+    />
   )
 }
-
-const WebionFooter = React.forwardRef<HTMLDivElement, Record<string, never>>((_, ref) => {
-  const { t } = useTranslation();
-  
-  const typoSx = {
-    textAlign: "left",
-    width: "fit-content",
-    margin: 0,
-  };
-
-  return (
-    <Footer ref={ref}>
-      <FooterContent>
-        <FooterColumn StackProps={{ alignItems: {xs:'center', md: "baseline"}}}>
-          <Typography variant="subtitle2">
-            HOME
-          </Typography>
-          <FooterBtn
-            color="info"
-            href="/#home"
-          >
-            {t("navbar-button1")}
-          </FooterBtn>
-          <FooterBtn
-            color="info"
-            href="/#services"
-          >
-            {t("navbar-button2")}
-          </FooterBtn>
-          <FooterBtn
-            color="info"
-            href="/#projects"
-          >
-            {t("navbar-button3")}
-          </FooterBtn>
-          <FooterBtn
-            color="info"
-            href="/#contacts"
-          >
-            {t("navbar-button5")}
-          </FooterBtn>
-        </FooterColumn>
-        <FooterColumn StackProps={{ alignItems: {xs:'center', md: "baseline"}, }} >
-          <Typography variant="subtitle2" sx={typoSx}>
-          {t("contact-us-title")}
-          </Typography>
-          <FooterBtn 
-            color="info" 
-            onClick={() => window.open("http://maps.google.com/?q=Webion SRL", '_blank')?.focus()}
-          >
-            via Panfilo Castaldi 3, Modena
-          </FooterBtn>
-          <FooterBtn 
-            color="info" 
-            href="tel:+39 389 008 6632"
-          >
-            +39 389 008 6632
-          </FooterBtn>
-          <FooterBtn
-            color="info" 
-            href="mailto:amministrazione@webion.it"
-          >
-            amministrazione@webion.it
-          </FooterBtn>
-        </FooterColumn>
-        <FooterColumn 
-          StackProps={{ 
-            alignItems: {xs:'center', md: "left"}, 
-          }} 
-        >
-          <Typography variant="subtitle2">
-            SOCIAL
-          </Typography>
-          <Grid 
-            container 
-            sx={{justifyContent: "center"}}
-          >
-            <IconButton 
-              aria-label="facebook" 
-              size="large" 
-              href="https://www.facebook.com/Webion-107914721922394" 
-              target="_blank" 
-              color="primary"
-            >
-              <FacebookIcon fontSize="inherit" />
-            </IconButton>
-            <IconButton 
-              aria-label="facebook" 
-              size="large" 
-              href="https://www.quora.com/profile/Webion" 
-              target="_blank" 
-              color="primary"
-            >
-              <Img
-                width="25px"
-                height="20px"
-                src="/assets/images/quoraIcon.svg"
-              />
-            </IconButton>
-            <IconButton 
-              aria-label="instagram" 
-              size="large" 
-              href="https://www.instagram.com/webion.it/" 
-              target="_blank" 
-              color="primary"
-            >
-              <InstagramIcon fontSize="inherit" />
-            </IconButton>
-            <IconButton 
-              aria-label="linkedin" 
-              size="large" 
-              href="https://www.linkedin.com/company/webion-srl/about/" 
-              target="_blank" 
-              color="primary"
-            >
-              <LinkedInIcon fontSize="inherit" />
-            </IconButton>
-            <IconButton 
-              aria-label="github" 
-              size="large" 
-              href="https://github.com/webion-hub" 
-              target="_blank" 
-              color="primary"
-            >
-              <GitHubIcon fontSize="inherit" />
-            </IconButton>
-          </Grid>
-        </FooterColumn>
-      </FooterContent>
-      <Divider sx={{ width: "90%", margin: 'auto' }}/>
-      <FooterBottomLabel
-        TypographyProps={{ variant: "caption" }}
-      >
-        <Link
-          href="/policies-licenses" 
-          target="_blank"
-          sx={{ marginRight: 1 }}
-          color="inherit"
-        >
-          {t("privacy-link")}
-        </Link>
-        {t("p-iva")}
-        <Link
-          sx={{ marginLeft: 1 }}
-          color="inherit"
-          href="mailto:webionsrl@legalmail.it"
-        >
-          webionsrl@legalmail.it
-        </Link>
-      </FooterBottomLabel>
-    </Footer>
-  );
-})
-
-export default WebionFooter
--- libs/wui/src/index.ts
+++ libs/wui/src/index.ts
@@ -32,6 +32,7 @@ export * from './components/footer/Footer';
 export * from './components/footer/FooterBottomLabel';
 export * from './components/footer/FooterColumn';
 export * from './components/footer/FooterContent';
+export * from './components/footer/WuiFooter';
 
 export * from './components/side_bar/SideBar';
 export * from './components/side_bar/SideBarItem';
