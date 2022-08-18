e65042e8bbc92fcbd4e1e498a8c7262f481221c3
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-08-18 20:34:33 +0200

    Refactor test

--- libs/wshop/api/src/test/products/ProductEndpoint.spec.ts
+++ libs/wshop/api/src/test/products/ProductEndpoint.spec.ts
@@ -1,46 +1,40 @@
 import { faker } from "@faker-js/faker";
 import { api } from "../api"
 
+const createProduct = async () => {
+  const created = await api.products.create({
+    name: faker.commerce.product(),
+    code: faker.random.alphaNumeric(5),
+  });
+
+  return api.products.withId(created.data.id);
+}
+
+
 describe('Product endpoint', () => {
   faker.seed(101010);
 
-  const createNewProduct = () => {
-    return api.products.create({
-      name: faker.commerce.product(),
-      code: faker.random.alphaNumeric(5),
-    });
-  }
-
   it('Should get a product', async () => {
-    const created = await createNewProduct();
-
-    const product = await api.products
-      .withId(created.data.id)
-      .load();
-
-    expect(product).toMatchSnapshot();
-  });
-
-  it('Should delete a product', async () => {
-    const created = await createNewProduct();
-
-    const deleted = await api.products
-      .withId(created.data.id)
-      .delete();
-
-    expect(deleted).toMatchSnapshot();
+    const product = await createProduct();
+    const loaded = await product.load();
+    
+    expect(loaded).toMatchSnapshot();
   });
 
   it('Should update a product', async () => {
-    const created = await createNewProduct();
-
-    const updated = await api.products
-      .withId(created.data.id)
-      .update({
-        name: 'Paolo',
-        code: 'ABC123',
-      });
+    const product = await createProduct();
+    const updated = await product.update({
+      name: 'Paolo',
+      code: 'ABC123',
+    });
 
     expect(updated).toMatchSnapshot();
+  });
+
+  it('Should delete a product', async () => {
+    const product = await createProduct();
+    const deleted = await product.delete();
+
+    expect(deleted).toMatchSnapshot();
   });
 });
