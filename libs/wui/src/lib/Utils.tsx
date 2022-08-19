9cd78539679011ac2a5d3425670470ac28be0550
Stefano Calabretti <45517710+cala-br@users.noreply.github.com>
2022-08-19 15:19:35 +0200

    Update Utils.tsx

--- libs/wui/src/lib/Utils.tsx
+++ libs/wui/src/lib/Utils.tsx
@@ -17,6 +17,6 @@ export class Utils {
     if (i === 0)
       return `${bytes} ${sizes[i]})`
 
-      return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`
+    return `${(bytes / (1024 ** i)).toFixed(1)} ${sizes[i]}`
   }
 }
