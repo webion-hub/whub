7d3aaa5c38c2d5549f283d7742088728f6a55dd0
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-08-24 11:21:52 +0200

    Add handler

--- libs/apis/core/src/index.ts
+++ libs/apis/core/src/index.ts
@@ -2,3 +2,4 @@ export * from "./lib/util/StatusCode";
 export * from "./lib/abstractions/Endpoint";
 export * from "./lib/abstractions/ApiBase";
 export * from "./lib/settings/ApiConfig";
+export * from "./lib/util/Handlers"
--- libs/wshop/api/src/lib/requests/UpdateProductRequest.ts
+++ libs/wshop/api/src/lib/requests/UpdateProductRequest.ts
@@ -1,5 +1,6 @@
 export interface UpdateProductRequest {
   readonly name: string;
+  readonly categoryId?: number;
   readonly description?: string;
   readonly price?: number;
   readonly code: string;
