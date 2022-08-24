86e4fb23d3bc662e117d9588d4c8f3db29194fad
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-08-24 10:05:38 +0200

    Update contract type

--- libs/wshop/api/src/lib/endpoints/CategoriesEndpoint.ts
+++ libs/wshop/api/src/lib/endpoints/CategoriesEndpoint.ts
@@ -1,5 +1,6 @@
 import { Endpoint } from "@whub/apis/core";
 import {AxiosInstance} from "axios";
+import { Category } from "../model/Category";
 import {UpdateCategoryRequest} from "../requests/UpdateCategoryRequest";
 import { CategoryEndpoint } from "./CategoryEndpoint";
 
@@ -17,7 +18,7 @@ export class CategoriesEndpoint extends Endpoint {
   }
 
   list() {
-    return this.client.get<string[]>(this.url);
+    return this.client.get<Category[]>(this.url);
   }
 
   create(request: UpdateCategoryRequest) {
