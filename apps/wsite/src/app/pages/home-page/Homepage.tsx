c27019835e5a1357e577dcf0618f8bc38013c3b5
Alessandro Dodi <dodialessandro3@gmail.com>
2022-08-25 12:00:54 +0200

    Change hero section

--- apps/wsite/src/app/pages/home-page/Homepage.tsx
+++ apps/wsite/src/app/pages/home-page/Homepage.tsx
@@ -13,31 +13,53 @@ export default function Homepage() {
   const theme = useTheme()
 
   return (
-    <Page sx={{ marginTop: 0 }}>
-      <Sections>
+    <Page sx={{ marginTop: 0, backgroundColor: "black", margin: "Auto" }}>
+      <video
+        autoPlay
+        muted
+        loop
+        id="myVideo"
+        style={{
+          position: "fixed",
+          backgroundColor: "rgba(0,0,0,.5) !important",
+          width: '100vw',
+          height: '100vh',
+          maxHeight: '1200px',
+          objectFit: 'cover',
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
+        <source
+          src="../../../assets/images/background.webm"
+          type="video/mp4"
+          style={{
+            backgroundColor: "rgba(0,0,0,.5) !important",
+            width: "130%",
+          }}
+        />
+      </video>
+      <Sections
+        sx={{
+          backgroundImage: "url(../../../assets/images/business)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: "black",
+          margin: "auto",
+          maxWidth: "100vw !important"
+        }}
+      >
         <Section
-          id="home"
-          maxWidth="100%"
-          sx={{
-            padding: 0,
-            backgroundImage: {
-              xs: "url('assets/images/backgroundMobile-min.webp')",
-              md: "url('assets/images/background-min.webp')",
-            },
-            backgroundPosition: {
-              xs: "45% 50%",
-              md: "calc(50% + 300px) 0px",
-            },
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            minHeight: "800px",
-          }
-        }>
+          showBackground
+          backgroundSx={{ backgroundColor: "rgba(0,0,0,.5)" }}
+          sx={{ maxWidth: "100%", width: "100%", zIndex: 2 }}
+        >
           <Home />
         </Section>
         <Section
           id="about-us"
           maxWidth="100%"
+          showBackground
+          backgroundSx={{ backgroundColor: "black !important" }}
         >
           <AIDA />
         </Section>
@@ -45,6 +67,7 @@ export default function Homepage() {
           id="services"
           showBackground
           background={pcbBackground(theme)}
+          backgroundSx={{ backgroundColor: "black !important" }}
         >
           <Services />
         </Section>
