b46e1c482468214a58221ec22e4c97343a4b702e
Matteo2437 <matteo.budriesi@gmail.com>
2022-08-29 15:25:49 +0200

    wip

--- libs/wshop/api/src/lib/endpoints/SearchEndpoint.ts
+++ libs/wshop/api/src/lib/endpoints/SearchEndpoint.ts
@@ -2,15 +2,11 @@ import { Endpoint } from "@whub/apis-core";
 import { Product } from "../model/Product";
 
 export class SearchEndpoint extends Endpoint {
-  private readonly mapper: Product
-
   get url(): string {
     return `shop/products/search`;
   }
 
-  async search(request: SearchRequest) {
-    return this.client
-      .get<Product[]>(this.url, request)
-      .then(r => );
+  async search() {
+    return this.client.get<Product[]>(this.url);
   }
 }
