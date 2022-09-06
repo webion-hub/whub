0ca0f9e14ff994513c34462bf47b5a06c54cbed1
Matteo2437 <matteo.budriesi@gmail.com>
2022-09-06 19:04:09 +0200

    fix

--- libs/wshop/ui/src/components/inputs/ProductDescriptionInput.tsx
+++ libs/wshop/ui/src/components/inputs/ProductDescriptionInput.tsx
@@ -1,12 +1,16 @@
-import { InputValidator, TextEditor, Validators } from "@whub/wui";
+import { InputValidator, TextEditor, Utils, Validators } from "@whub/wui";
 
 export function ProductDescriptionInput() {
   return (
     <InputValidator
       name="description"
       value=''
-      validators={[Validators.max(4096)]}
-    >
+      validators={[
+        Validators.customValidator((description: string) => {
+          const desc = Utils.stripHtml(description ?? '')
+          return Validators.max(4096)(desc)
+        })
+      ]}    >
       <TextEditor
         label="Descrizione"
         maxCharacters={4096}
