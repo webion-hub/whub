c7da2a22d7ad06de14d096b3756fd788533c89bb
Matteo Budriesi <matteobudriesi@Matteos-MBP.homenet.telecomitalia.it>
2022-11-13 18:06:01 +0100

    Fix bug

--- apps/wsite/pages/studies/kaire.tsx
+++ apps/wsite/pages/studies/kaire.tsx
@@ -1,5 +1,5 @@
 import { EmojiObjectsRounded, ReportProblemRounded } from '@mui/icons-material';
-import { Typography } from '@mui/material';
+import { Typography, useTheme } from '@mui/material';
 import { Stack } from '@mui/system';
 import { Page, Section, Sections, useLanguage } from '@whub/wui';
 import { IconCard } from '../../components/IconCard';
@@ -8,12 +8,21 @@ import { GetAQuoteSection } from '../../components/sections/GetAQuote';
 
 export default function KaireCaseStudy() {
   const { t, tHtml } = useLanguage();
+  const theme = useTheme();
 
   return (
     <Page>
       <Sections>
         <Section>
-          <Stack direction="column" spacing={4} sx={{ width: '100%', marginTop: 4 }}>
+          <Stack
+            direction="column"
+            spacing={4}
+            sx={{
+              width: '100%',
+              marginTop: 4,
+              marginInline: 1
+            }}
+          >
             <Typography variant="h2" textAlign="center">
               Kaire Automation
             </Typography>
@@ -29,10 +38,15 @@ export default function KaireCaseStudy() {
         <Section>
           <Stack
             direction="row"
+            flexWrap="wrap"
             sx={{
               '& > *': {
                 margin: 1,
-                width: (theme) => `calc(50% - ${theme.spacing(2)})`,
+                minWidth: 'auto !important',
+                width: {
+                  xs: `calc(100% - ${theme.spacing(2)})`,
+                  md: `calc(50% - ${theme.spacing(2)})`
+                },
               },
             }}
           >
