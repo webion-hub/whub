8a91b04f711bce523842452a5db62d278674fe65
Matteo Budriesi <matteo.budriesi@webion.it>
2023-02-16 11:51:42 +0100

    update packages

--- scripts/include-package.sh
+++
@@ -1,5 +0,0 @@
-cd apps/$1
-
-tmp=$(mktemp)
-jq '.dependencies += { "'$2'": "workspace:*" }' package.json > "$tmp" && mv "$tmp" package.json
-pnpm i
--- scripts/remove-package.sh
+++
@@ -1,5 +0,0 @@
-cd apps/$1
-
-tmp=$(mktemp)
-jq 'del(.dependencies.'$2')' package.json > "$tmp" && mv "$tmp" package.json
-pnpm i
