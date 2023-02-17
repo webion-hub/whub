a72151ca826dc572eadb7efc0929d406d2a02bcf
Matteo Budriesi <matteo.budriesi@webion.it>
2023-02-17 09:44:58 +0100

    remove dist folders

--- packages/api-ui/blog/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { BlogApi } from '@webion/api-blog';
-
-declare const blogFactory: () => BlogApi;
-declare const useBlog: () => BlogApi;
-
-export { blogFactory, useBlog };
--- packages/api-ui/contactus/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { ContactUsApi } from '@webion/api-contactus';
-
-declare const contactUsFactory: () => ContactUsApi;
-declare const useContactUs: () => ContactUsApi;
-
-export { contactUsFactory, useContactUs };
