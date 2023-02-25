bfdbde19e604ad0862b00e2cc5c087a6ef2162a5
Matteo Budriesi <68251639+matteo2437@users.noreply.github.com>
2023-02-25 12:33:19 +0100

    use lazy imgs (#149)
    Co-authored-by: Matteo Budriesi <matteo.budriesi@webion.it>

--- apps/wsite/components/sections/WebionChosenBy.tsx
+++ apps/wsite/components/sections/WebionChosenBy.tsx
@@ -5,63 +5,63 @@ export default function WebionChosenBy(props: ISection) {
   return (
     <ChosenBy id={props.id}>
       <NextImg
-        priority
+        loading="lazy"
         auto={{ height: '28px' }}
         sizes="25vw"
         alt="bocconi"
         src="/assets/images/clients/gianos.png"
       />
       <NextImg
-        priority
+        loading="lazy"
         auto={{ height: '28px' }}
         sizes="25vw"
         alt="simm"
         src="/assets/images/clients/simm.png"
       />
       <NextImg
-        priority
+        loading="lazy"
         auto={{ height: '28px' }}
         sizes="25vw"
         alt="bocconi"
         src="/assets/images/clients/bocconi.png"
       />
       <NextImg
-        priority
+        loading="lazy"
         auto={{ height: '28px' }}
         sizes="25vw"
         alt="kaire"
         src="/assets/images/clients/kaire.png"
       />
       <NextImg
-        priority
+        loading="lazy"
         auto={{ height: '28px' }}
         sizes="25vw"
         alt="LCI Agency"
         src="/assets/images/clients/lci-agency.png"
       />
       <NextImg
-        priority
+        loading="lazy"
         auto={{ height: '28px' }}
         sizes="25vw"
         alt="mentorz"
         src="/assets/images/clients/mentorz.png"
       />
       <NextImg
-        priority
+        loading="lazy"
         auto={{ height: '28px' }}
         sizes="25vw"
         alt="massyve"
         src="/assets/images/clients/massyve.png"
       />
       <NextImg
-        priority
+        loading="lazy"
         auto={{ height: '28px' }}
         sizes="25vw"
         alt="The Pink Palace"
         src="/assets/images/clients/pink-palace.png"
       />
       <NextImg
-        priority
+        loading="lazy"
         auto={{ height: '28px' }}
         sizes="25vw"
         alt="The Pink Palace"
